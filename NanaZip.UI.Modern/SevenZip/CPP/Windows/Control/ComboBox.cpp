34751b4451040fac75dc3eefcb5b0e9c130f147c
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-04-24 17:18:40 +0800

    Update ComboBox.cpp

--- NanaZip.UI.Modern/SevenZip/CPP/Windows/Control/ComboBox.cpp
+++ NanaZip.UI.Modern/SevenZip/CPP/Windows/Control/ComboBox.cpp
@@ -1,16 +1,8 @@
-﻿// Windows/Control/ComboBox.cpp
+// Windows/Control/ComboBox.cpp
 
 #include "StdAfx.h"
-
-#ifndef _UNICODE
-#include "../../Common/StringConvert.h"
-#endif
-
+#include "OSVersion.h"
 #include "ComboBox.h"
-
-#ifndef _UNICODE
-extern bool g_IsNT;
-#endif
 
 namespace NWindows {
 namespace NControl {
@@ -18,12 +10,12 @@ namespace NControl {
 LRESULT CComboBox::GetLBText(int index, CSysString &s)
 {
   s.Empty();
-  LRESULT len = GetLBTextLen(index); // length, excluding the terminating null character
+  LRESULT len = GetLBTextLen(index); 
   if (len == CB_ERR)
-    return len;
+    return CB_ERR;
   LRESULT len2 = GetLBText(index, s.GetBuf((unsigned)len));
   if (len2 == CB_ERR)
-    return len;
+    return CB_ERR;
   if (len > len2)
     len = len2;
   s.ReleaseBuf_CalcLen((unsigned)len);
@@ -45,10 +37,10 @@ LRESULT CComboBox::GetLBText(int index, UString &s)
   {
     LRESULT len = SendMsgW(CB_GETLBTEXTLEN, MY__int_TO_WPARAM(index), 0);
     if (len == CB_ERR)
-      return len;
+      return CB_ERR;
     LRESULT len2 = SendMsgW(CB_GETLBTEXT, MY__int_TO_WPARAM(index), (LPARAM)s.GetBuf((unsigned)len));
     if (len2 == CB_ERR)
-      return len;
+      return CB_ERR;
     if (len > len2)
       len = len2;
     s.ReleaseBuf_CalcLen((unsigned)len);
@@ -57,7 +49,7 @@ LRESULT CComboBox::GetLBText(int index, UString &s)
   AString sa;
   LRESULT len = GetLBText(index, sa);
   if (len == CB_ERR)
-    return len;
+    return CB_ERR;
   s = GetUnicodeString(sa);
   return s.Len();
 }
